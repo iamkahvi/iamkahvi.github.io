badb750fa17c14222ff69e0d9bbd4a62e108908c
Kahvi Patel <iamkahvi@gmail.com>
2019-02-12 22:44:13 -0800

    now i can change elements with js!

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World';
+var myImage = document.querySelector('.booklist > img');
 
-var myImage = document.querySelector('img');
-
-myImage.onclick = function () {
+/* myImage.onclick = function () {
     var mySrc = myImage.getAttribute('src');
     if (mySrc === '/assets/booklist2.png') {
         myImage.setAttribute ('src', '/assets/rainbow.png');
     } else {
-        myImage.setAttribute ('src',  '/assets/booklist2.png');
+        myImage.setAttribute ('src', '/assets/booklist2.png');
     }
+} */
+
+/* myImage.onmouseover = function() {
+    myImage.setAttribute ('src', '/assets/rainbow.png');
 }
 
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('.title');
+myImage.onmouseout = function() {
+    myImage.setAttribute ('src', '/assets/booklist2.png');
+} */
 
-function setUserName() {
+var myTitle = document.querySelector('.title');
+var theOriginal = myTitle.innerText;
+
+myTitle.onmouseover = function() {
+    myTitle.textContent = 'hello';
+}
+
+myTitle.onmouseout = function() {
+    myTitle.textContent = theOriginal;
+}
+
+/* function setUserName() {
     var myName = prompt('Plz enter your name.');
     localStorage.setItem('name', myName);
     myHeading.textContent = 'This is your name, ' + myName;
@@ -30,4 +43,4 @@ if (!localStorage.getItem('name')) {
 
 myButton.onclick = function() {
     setUserName();
-}
+} */
