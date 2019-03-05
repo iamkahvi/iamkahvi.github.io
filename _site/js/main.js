5f85d1c1e71389831401690da75bc82688cf1444
Kahvi Patel <iamkahvi@gmail.com>
2019-03-05 13:03:55 -0800

    now to title says hello!

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,23 +1,6 @@
 var myImage = document.querySelector('.booklist > img');
 
-/* myImage.onclick = function () {
-    var mySrc = myImage.getAttribute('src');
-    if (mySrc === '/assets/booklist2.png') {
-        myImage.setAttribute ('src', '/assets/rainbow.png');
-    } else {
-        myImage.setAttribute ('src', '/assets/booklist2.png');
-    }
-} */
-
-/* myImage.onmouseover = function() {
-    myImage.setAttribute ('src', '/assets/rainbow.png');
-}
-
-myImage.onmouseout = function() {
-    myImage.setAttribute ('src', '/assets/booklist2.png');
-} */
-
-var myTitle = document.querySelector('.title');
+var myTitle = document.querySelector('.title > span');
 var theOriginal = myTitle.innerText;
 
 myTitle.onmouseover = function() {
@@ -27,20 +10,3 @@ myTitle.onmouseover = function() {
 myTitle.onmouseout = function() {
     myTitle.textContent = theOriginal;
 }
-
-/* function setUserName() {
-    var myName = prompt('Plz enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'This is your name, ' + myName;
-}
-
-if (!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'This is your name, ' + storedName;
-}
-
-myButton.onclick = function() {
-    setUserName();
-} */
