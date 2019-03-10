ae51e8bab098b9484b09e8272d1ab347586245b1
Kahvi Patel <iamkahvi@gmail.com>
2019-03-10 16:22:26 -0700

    put in a search bar that works!

--- _site/js/main.js
+++ _site/js/main.js
@@ -4,9 +4,27 @@ var myTitle = document.querySelector('.title > span');
 var theOriginal = myTitle.innerText;
 
 myTitle.onmouseover = function() {
-    myTitle.textContent = 'hello';
+    myTitle.textContent = 'helloo!';
 }
 
 myTitle.onmouseout = function() {
     myTitle.textContent = theOriginal;
 }
+
+var searchBar = document.querySelector('input');
+
+//searchBar.onclick = searchPosts;
+
+function searchPosts() {
+    const posts = document.querySelectorAll('.c-archives__item');
+    const text = searchBar.value.toLowerCase();
+    for (const post of posts) {
+        var title = post.querySelector('b').innerText;
+        if (title.toLowerCase().indexOf(text) > -1) {
+            post.style.display = "";
+        } else {
+            post.style.display = "none";
+        }
+    }
+}
+
