2dc5cfb2b1557da79d6cf7c6ddf9c78223e72bee
Kahvi Patel <iamkahvi@gmail.com>
2019-03-12 16:44:28 -0700

    the title js thing was sort of annoying

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,12 +1,10 @@
-var myImage = document.querySelector('.booklist > img');
-
 var myTitle = document.querySelector('.title > span');
 var theOriginal = myTitle.innerText;
 
-myTitle.onmouseover = function() {
-    myTitle.textContent = 'hello';
-}
+//myTitle.onmouseover = function() {
+    //myTitle.textContent = 'hello';
+//}
 
-myTitle.onmouseout = function() {
-    myTitle.textContent = theOriginal;
-}
+//myTitle.onmouseout = function() {
+    //myTitle.textContent = theOriginal;
+//}
