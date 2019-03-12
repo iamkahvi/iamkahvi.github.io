9d86316ff0219d6e5ab5ea2a32cbe03e3cc1395e
Kahvi Patel <iamkahvi@gmail.com>
2019-03-11 21:22:34 -0700

    now the years are hidden and filtered !!

--- _site/js/main.js
+++ _site/js/main.js
@@ -11,29 +11,49 @@ myTitle.onmouseout = function() {
     myTitle.textContent = theOriginal;
 }
 
-var searchBar = document.querySelector('input');
 
 searchBar.onkeyup = searchPosts;
 
 function searchPosts() {
+    var searchBar = document.querySelector('input');
+    console.log("searchPosts");
     const posts = document.querySelectorAll('.c-archives__item');
     const text = searchBar.value.toLowerCase();
 
-    document.querySelectorAll('.c-archives__year').forEach(function (element) {
-        element.style.display = "none";
+    document.querySelectorAll('.c-archives__year').forEach(function (year) {
+        year.style.display = "none";
     });
+    
     for (const post of posts) {
         var title = post.querySelector('b').innerText;
         if (title.toLowerCase().indexOf(text) > -1) {
             post.style.display = "";
+            post.parentElement.previousElementSibling.style.display = "";
         } else {
             post.style.display = "none";
         }
     }
-    if (text === "") {
-       document.querySelectorAll('.c-archives__year').forEach(function (element) {
-           element.style.display = "";
-       });
-    }
 }
 
+function searchBooks() {
+    var searchBar = document.querySelector('input');
+    console.log("searchBooks");
+    const books = document.querySelectorAll('li');
+    const text = searchBar.value.toLowerCase();
+    const years = document.querySelectorAll('.c-article__main h2');
+    var showTitle = false;
+
+    years.forEach(function (year) {
+        year.style.display = "none";
+    });
+    
+    for (const book of books) {
+        var title = book.querySelector('b').innerText;
+        if (title.toLowerCase().indexOf(text) > -1) {
+            book.style.display = "";
+            book.parentElement.previousElementSibling.style.display = "";
+        } else {
+            book.style.display = "none";
+        }
+    }
+}
