11943444bc6f6ddda891e1e320fcf16281b72014
Kahvi Patel <iamkahvi@gmail.com>
2019-03-16 14:54:51 -0700

    the search triggers when enter key is pressed or value is blank

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,14 +1,25 @@
+function search() {
+	var searchBar = document.querySelector('input');
 
-function searchPosts() {
-    var searchBar = document.querySelector('input');
+	if (event.keyCode == 13 || searchBar.value === "") {
+		if (searchBar.className === "posts") {
+			searchPosts(searchBar);
+		} 
+
+		if (searchBar.className === "books") {
+			searchBooks(searchBar);
+		} 
+	}
+		
+}
+
+function searchPosts(searchBar) {
+    //var searchBar = document.querySelector('input');
     const posts = document.querySelectorAll('.c-archives__item');
     const text = searchBar.value.toLowerCase();
+	const years = document.querySelectorAll('.c-archives__year'); 
 
-    if (event.keyCode == 13) {
-        searchBar.blur();
-    }
-
-    document.querySelectorAll('.c-archives__year').forEach(function (year) {
+    years.forEach(function (year) {
         year.style.display = "none";
     });
     
@@ -22,25 +33,25 @@ function searchPosts() {
         }
 
     }
-}
-
-function searchBooks() {
-    var searchBar = document.querySelector('input');
-    const books = document.querySelectorAll('li');
-    const text = searchBar.value.toLowerCase();
-    const years = document.querySelectorAll('.c-article__main h2');
-    var showTitle = false;
 
     if (event.keyCode == 13) {
         searchBar.blur();
     }
+
+}
+
+function searchBooks(searchBar) {
+    //var searchBar = document.querySelector('input');
+    const books = document.querySelectorAll('li');
+    const text = searchBar.value.toLowerCase();
+    const years = document.querySelectorAll('.c-article__main h2');
 
     years.forEach(function (year) {
         year.style.display = "none";
     });
     
     for (const book of books) {
-        var title = book.querySelector('b').innerText;
+        var title = book.innerText;
         if (title.toLowerCase().indexOf(text) > -1) {
             book.style.display = "";
             book.parentElement.previousElementSibling.style.display = "";
@@ -48,5 +59,10 @@ function searchBooks() {
             book.style.display = "none";
         }
     }
+
+    if (event.keyCode == 13) {
+        searchBar.blur();
+    }
+
 
 }
