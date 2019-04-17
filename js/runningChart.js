8e4999b8de2c2097b42f85f94e15dd6545f6d30a
Kahvi Patel <iamkahvi@gmail.com>
2019-04-17 15:27:08 -0700

    importing data from local json file now becuase auth is a pain for Strava API

--- js/runningChart.js
+++ js/runningChart.js
@@ -1,25 +1,62 @@
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://www.strava.com/api/v3/athlete/activities?after=1271479753",
-  "method": "GET",
-  "headers": {
-    "Authorization": "Bearer f5709e32dc1d9eb636edef067a59b99d4b7994ff",
-    "cache-control": "no-cache",
-    "Postman-Token": "6c060c17-cfbd-472d-9f92-e45f971708a7"
-  }
+//var settings = {
+  //"async": true,
+  //"crossDomain": true,
+  //"url": "https://www.strava.com/api/v3/athlete/activities?after=1271479753",
+  //"dataType": "json",
+  //"method": "GET",
+  //"headers": {
+    //"Authorization": "Bearer 0d5ed8a354f23c14dd5fe8b722bb2b9dfacce6d0",
+    //"cache-control": "no-cache",
+    //"Postman-Token": "36cc36b9-348f-4144-b5fb-eb76a56551a4"
+  //}
+//}
+
+//activities = [];
+
+//$.ajax(settings).done(function (response) {
+  //console.log(response);
+  //console.log(typeof response);
+  //activities = response;
+//});
+
+var data2;
+
+window.onload = function() {
+  $.getJSON('/data/data.json', function(json) {
+	data2 = json;
+	drawLineChart();
+  });
 }
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-  var activities = response;
-});
+function drawLineChart() {
 
-const dates = activities.map(element => element.start_date);
-const distances = activities.map(element => element.distance);
+  // Loading local json file
 
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: options
-})
+  var ctx = document.getElementById('myChart').getContext('2d');
+
+  //console.log(data2);
+
+  dates = data2.map(element => moment(element.start_date).format('MMM YYYY'));
+  distances = data2.map(element => element.distance);
+
+  //console.log(dates);
+  //console.log(distances);
+
+  var myChart = new Chart(ctx, {
+	  type: 'line',
+	  data: {
+		labels: dates, 
+		datasets: [{
+		  label: "Distance Run",
+		  data: distances
+		}]
+	  },
+	  options: {
+		  title: {
+			  display: true,
+			  text: 'Custom Chart Title'
+		  }
+	  }
+  });
+}
+
